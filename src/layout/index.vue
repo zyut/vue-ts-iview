564a3036c89a3c0f2fbfd9b33f9304ffa248ce63
张玉婷 <yuting.zhang@seetatech.com>
2020-07-06 17:50:21 +0800

    初始化项目

---
+++ src/layout/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+export default class Index extends Vue {}
+</script>
+
+<style scoped>
+
+</style>
